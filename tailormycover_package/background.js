0c866956debf822a9d65c1d4d869832f94682cf6
Akshat Boudh <166040413+akboudh@users.noreply.github.com>
2025-11-22 22:03:10 -0500

    Delete tailormycover_package directory

--- tailormycover_package/background.js
+++
@@ -1,7 +0,0 @@
-// Background service worker
-// Currently minimal as most logic is in popup and content script.
-// Can be used for context menus or handling installation events in the future.
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Cover Letter Generator extension installed.");
-});
